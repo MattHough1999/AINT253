698c7882185e6cfea0e3c65a2be3c58d10811051
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2020-01-08 15:57:43 +0000

    tweaks

--- EscapeRoom/Assets/Scripts/CanvasManager.cs
+++ EscapeRoom/Assets/Scripts/CanvasManager.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class CanvasManager : MonoBehaviour
 {
-    
+    public bool boolean = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
         if (SceneManager.GetActiveScene().name == "Menu" || SceneManager.GetActiveScene().name == "endGame" ) 
         {
             //btnResume.enabled = false;
@@ -20,26 +20,30 @@ public class CanvasManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
+        if(boolean == true) { Cursor.visible = true; Cursor.lockState = CursorLockMode.None; }
+        if (Input.GetKeyDown(KeyCode.Tab)) 
         {
             resume();
         }
+        if (Input.GetKeyDown(KeyCode.R)) levelOne();
     }
     public void resume() 
     {
-        if(GetComponent<Canvas>().enabled == true) 
+        
+        if (GetComponent<Canvas>().enabled == true) 
         {
+            boolean = false;
             GetComponent<Canvas>().enabled = false;
         }
-        else { GetComponent<Canvas>().enabled = true; }
+        else { GetComponent<Canvas>().enabled = true; Cursor.visible = true; boolean = true; }
     }
     public void levelOne() 
     {
-        SceneManager.LoadScene("room1");
+        SceneManager.LoadScene("Room1");
     }
     public void levelTwo()
     {
-        SceneManager.LoadScene("room2");
+        SceneManager.LoadScene("Room2");
     }
     public void returnToMenu()
     {
